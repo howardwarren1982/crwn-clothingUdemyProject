1737cb93841a8fbd4a705ef85dd6d17a77064bc6
howardwarren1982 <howardwarren89@gmail.com>
2022-04-11 21:21:04 -0400

    little fix

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-
-const config = {
-  apiKey: "AIzaSyDMkpaDOiZDvSDLi9qOB8M2eylAmfxaYf4",
-  authDomain: "crwn-db-56da8.firebaseapp.com",
-  projectId: "crwn-db-56da8",
-  storageBucket: "crwn-db-56da8.appspot.com",
-  messagingSenderId: "146519584752",
-  appId: "1:146519584752:web:f4c81634ae8fd77199b735",
-  measurementId: "G-N8FLHQRS0P",
-};
+import config from "../variables";
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
